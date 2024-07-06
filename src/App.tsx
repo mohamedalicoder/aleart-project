97cf27142709c0ec8d760cdbded4d66317df94ec
mohamed ali_coder <42010447.mohamed@acu.edu.eg>
2024-07-06 15:20:58 +0300

    add scss styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import './App.css';
+import Alert from './components/ui/Alert/Alert';
+import { BellRing } from 'lucide-react';
+import { NotebookPen } from 'lucide-react';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Alert title='Alert Title' description='Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Vitae eum saepe praesentium ullam magnam dolorum ut eos a 
+        ipsam sint. A, odio minima? Velit aliquid sunt fuga accusamus,
+         beatae aliquam.' icon={BellRing} styles="alert-wrapper" />
+
+         
+      <Alert title='Alert Title2' description='Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Vitae eum saepe praesentium ullam magnam dolorum ut eos a 
+        ipsam sint. A, odio minima? Velit aliquid sunt fuga accusamus,
+         beatae aliquam.' icon={NotebookPen} styles="alert-2"  />
+
+      <Alert title='Alert Title3' description='Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Vitae eum saepe praesentium ullam magnam dolorum ut eos a 
+        ipsam sint. A, odio minima? Velit aliquid sunt fuga accusamus,
+         beatae aliquam.' icon={BellRing} styles="2px dotted blue"  />
+
+      <Alert title='Alert Title4' description='Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Vitae eum saepe praesentium ullam magnam dolorum ut eos a 
+        ipsam sint. A, odio minima? Velit aliquid sunt fuga accusamus,
+         beatae aliquam.' icon={BellRing} styles="2px dotted red"  />
+
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ui/Alert/Alert.tsx
+++ src/components/ui/Alert/Alert.tsx
@@ -1,13 +1,31 @@
-interface IProps{
-title:string
-}
-const Alert = ({title}:IProps) => {
-  return (
-    <div>
-      {title}
+import "./index.scss";
 
+import { ReactElement } from 'react';
+import { X } from 'lucide-react';
+
+interface IProps {
+  title: string;
+  icon: React.ComponentType; // Change icon type to ComponentType
+  description: string;
+  styles?: string; // Add styles prop to accept custom styles
+}
+const Alert = ({ title, description, icon: Icon,styles }: IProps): ReactElement => { // Rename icon to Icon
+  return (
+    <div  className={styles}>
+      <div className="titles">
+        <div style={{display:"flex" , justifyContent:"space-between" , alignItems:"center" , width:"120px"}}>
+        <Icon /> {/* Use Icon as a component */}
+        <h4>{title}</h4>
+        </div>
+     
+      <span>
+      <X />
+      </span>
+      </div>
+     
+      <p>{description}</p>
     </div>
-  )
+  );
 }
 
 export default Alert
